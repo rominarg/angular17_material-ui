eef680ba24af0d1842b17c73c0a717aa53d6e139
RominaNavarrete <rominagnavarrete@gmail.com>
2024-04-19 21:32:11 -0300

    datepickerModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MaterialModule } from './modules/material/material.module';
+import { DatePickerComponent } from './components/date-picker/date-picker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatePickerComponent
   ],
   imports: [
     BrowserModule,
--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+//Modulos empresados de Angular Material
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports:[
+    MatDatepickerModule
   ]
 })
 export class MaterialModule { }
